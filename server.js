a4239d69209266e97a859e636b413b5e689c0d83
John Alexis Guerra Gómez <john.guerra@gmail.com>
2021-10-19 19:31:55 -0700

    Connecting to the db

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+async function getFires() {
+  const db = await open({
+    filename : "./db/fires.db",
+    driver: sqlite3.Database
+  });
+  console.log("Connected to db");
+}
 
 
-console.log("Hola mundo!");
+getFires();
